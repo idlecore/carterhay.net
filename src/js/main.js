0469ce8847231c12115dd9e361fee13e0c2712f2
Carter Hay <clccmh@gmail.com>
2017-08-09 23:49:04 -0500

    removed unneeded js

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,4 @@
  **/
 
 <!--
-
-function load_navbar() {
-  //console.log("Loaded main.js");
-  $("#navbar").load("navbar.html", console.log("Loaded navbar"));
-}
-
-
-$(function() {
-  load_navbar();
-});
-
-
-//$(function() {
-//  console.log("Loaded main.js");
-//  $("#navbar").load("navbar.html");
-//});
-
 -->
